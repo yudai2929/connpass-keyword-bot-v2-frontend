551317bf87ddc806ee8efdad1ca67efb4833f471
yudai2929 <yudai201729@icloud.com>
2023-11-29 01:24:50 +0900

    👍add: chakra ui

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+// app/providers.tsx
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
